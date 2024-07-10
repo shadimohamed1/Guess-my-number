533e68742223497f0466d0233b4c839e8a857831
Shadi mohamed <132169584+shadimohamed1@users.noreply.github.com>
2024-07-10 16:48:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+`use strict`;
+// const should be initialized in the same line
+let answer = Math.trunc(Math.random() * 20) + 1;
+let guess;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".top").textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  guess = Number(document.querySelector(".sc").value);
+
+  if (guess === answer) {
+    document.querySelector(".question").textContent = answer;
+    displayMessage("congratulations");
+    //document.querySelector(".top").textContent = "Congratulations";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".question").style.width = "30rem";
+    document.querySelector(".question").style.height = "10rem";
+    if (score >= highscore) {
+      highscore = score;
+      document.querySelector(".but").textContent = `Highscore : ${highscore}`;
+    }
+    score = 20;
+  } else if (guess !== answer) {
+    if (score > 1) {
+      displayMessage(guess > answer ? "Too high!" : "Too low!");
+      // document.querySelector(".top").textContent =
+      //   guess > answer ? "Too high!" : "Too low!";
+      score--;
+      document.querySelector(".score-span").textContent = score;
+    } else {
+      document.querySelector(
+        ".top"
+      ).textContent = `You lose the game Press on again!`;
+      document.querySelector(".score-span").textContent = 0;
+    }
+  } else if (!guess) {
+    document.querySelector(".top").textContent = "There is no guess!";
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  answer = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector(".sc").textContent = ` `;
+  document.querySelector(".top").textContent = "Start guessing .. ";
+  document.querySelector(".score-span").textContent = score;
+  document.querySelector(".question").textContent = `?`;
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".question").style.width = "100px";
+  document.querySelector(".question").style.height = "50px";
+});
